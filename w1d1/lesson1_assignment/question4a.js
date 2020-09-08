0af7f7f42f0ca3ae48a0a82c48ecc30ed4267bee
Hung Le <dangky1410@gmail.com>
2020-09-07 21:43:16 -0500

    update lesson 1 assignment

--- w1d1/lesson1_assignment/question4a.js
+++ w1d1/lesson1_assignment/question4a.js
@@ -11,7 +11,7 @@ return new_arr;
 }
 
 
-let arr = [0, 1, 2, 3, 4, 5]
+let arr = [1, 2, 3, 4, 5]
 
 let output = reverseArray(arr);
 
--- w1d1/lesson1_assignment/question4b.js
+++ w1d1/lesson1_assignment/question4b.js
@@ -19,6 +19,6 @@ function reverseArrayInPlace(arr){
     }
     
     
-    let arr =[0, 1, 2, 3, 4, 5];
+    let arr =[1, 2, 3, 4, 5];
     let output = reverseArrayInPlace(arr);
     alert(output);
