6a21e5b8fe0e37a983ac1c14ad2882365f9654ce
Hung Le <dangky1410@gmail.com>
2020-09-08 14:15:06 -0500

    lesson 1 assignment update

--- w1d1/array_assignment/array_question5.js
+++ w1d1/array_assignment/array_question5.js
@@ -1,12 +1,21 @@
 function getMaxSubSum(arr) {
-  let maxSum = 0;
-  let partialSum = 0;
 
-  for (let item of arr) {
-    partialSum += item;
-    maxSum = Math.max(maxSum, partialSum);
-    if (partialSum < 0) partialSum = 0;
+  let maxSum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    let partialSum = 0;
+    for (let j = i; j < arr.length; j++) {
+
+      partialSum += arr[j];
+      maxSum = Math.max(partialSum, maxSum);
+
+
+    }
+
   }
 
   return maxSum;
 }
+
+let arr =[-1, 2, 3, -9];
+let output = getMaxSubSum(arr);
+alert(output);
