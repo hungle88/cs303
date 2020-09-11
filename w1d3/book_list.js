d71764bd8c76020dd17b164cce2c43c8b8f8ff6c
Hung Le <dangky1410@gmail.com>
2020-09-10 19:11:54 -0500

    Update book_list.js

--- w1d3/book_list.js
+++ w1d3/book_list.js
@@ -10,50 +10,44 @@ let library = [
   },
 ];
 
-function addBooks(){
-let book = {};
+function addBooks() {
+  let book = {};
   book.title = document.getElementById("new_title").value;
   book.author = document.getElementById("new_author").value;
-  book.libraryID = document.getElementById("new_id").value;
+  book.libraryID = +document.getElementById("new_id").value;
 
   library.push(book);
-
 }
 
-
-function showTitles(){
-    let allTitles =""
-    for(let i = 0; i < library.length; i++){
-        let newLine ="\r\n";
-        allTitles += library[i].title;
-        allTitles += newLine;
-    }
-    let textArea = document.getElementById("information");
-    textArea.innerHTML = allTitles;
-
+function showTitles() {
+  let allTitles = "";
+  for (let i = 0; i < library.length; i++) {
+    let newLine = "\r\n";
+    allTitles += library[i].title;
+    allTitles += newLine;
+  }
+  let textArea = document.getElementById("information");
+  textArea.innerHTML = allTitles;
 }
 
-function showAuthors(){
-    let allAuthors =""
-    for(let i = 0; i < library.length; i++){
-        let newLine ="\r\n";
-        allAuthors += library[i].author;
-        allAuthors += newLine;
-    }
-    let textArea = document.getElementById("information");
-    textArea.innerHTML = allAuthors;
-
+function showAuthors() {
+  let allAuthors = "";
+  for (let i = 0; i < library.length; i++) {
+    let newLine = "\r\n";
+    allAuthors += library[i].author;
+    allAuthors += newLine;
+  }
+  let textArea = document.getElementById("information");
+  textArea.innerHTML = allAuthors;
 }
 
-function showID(){
-    let allID =""
-    for(let i = 0; i < library.length; i++){
-        let newLine ="\r\n";
-        allID += library[i].libraryID;
-        allID += newLine;
-    }
-    let textArea = document.getElementById("information");
-    textArea.innerHTML = allID;
-
+function showID() {
+  let allID = "";
+  for (let i = 0; i < library.length; i++) {
+    let newLine = "\r\n";
+    allID += library[i].libraryID;
+    allID += newLine;
+  }
+  let textArea = document.getElementById("information");
+  textArea.innerHTML = allID;
 }
-
