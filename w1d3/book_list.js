f85b9e84cc422291ca6651dfff02f6666b42d7a7
Hung Le <dangky1410@gmail.com>
2020-09-10 19:31:53 -0500

    Update book_list.js

--- w1d3/book_list.js
+++ w1d3/book_list.js
@@ -2,15 +2,7 @@
 
 "use strict";
 
-let library = [
-  { title: "The Road Ahead", author: "Bill Gates", libraryID: 1254 },
-  { title: "Walter Isaacson", author: "Steve Jobs", libraryID: 4264 },
-  {
-    title: "Mockingjay: The Final Book of The Hunger Games",
-    author: "Suzanne Collins",
-    libraryID: 3245,
-  },
-];
+let library = [];
 /**This function add new books to the library
  *
  * @returns{object} return and add a new object to the library
