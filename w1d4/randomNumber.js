c8be153dc73c0156aa6e32ce38b7f5502b8e9c90
Hung Le <dangky1410@gmail.com>
2020-09-13 12:05:55 -0500

    update lesson 4 assignment

--- w1d4/randomNumber.js
+++ w1d4/randomNumber.js
@@ -1,7 +1,20 @@
 "use strict";
+/**
+ * 
+ * @param {number} min the minimum numnber 
+ * @param {number} max the maximum number
+ * 
+ * @returns {number} the random floating-point number between min and max
+ */
+function randomNumber(min, max) {
 
-function(min, max) {
-
-
+let randomValue = max - min; //Value between min and max
+let randomFloatingValue = Math.random() * randomValue; // random floating-point value between min and max
+let output = min + randomFloatingValue; // random floating-point number start from min and is less than max
+return output;
     
 }
+
+
+let rand =randomNumber(1, 5);
+alert(rand);
