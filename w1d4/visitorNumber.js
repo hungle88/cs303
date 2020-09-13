7da520974752a9f0c57e8dad3f5db7dccd64fec4
Hung Le <dangky1410@gmail.com>
2020-09-13 10:43:32 -0500

    update lesson 4 assignment

--- w1d4/visitorNumber.js
+++ w1d4/visitorNumber.js
@@ -1,10 +1,9 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-redeclare */
 "use strict";
-const prompt = require("prompt-sync")();
 
 let firstNum = +prompt("Please enter first number: ");
 let secondNum = +prompt("Please enter second number: ");
 
 let output = firstNum + secondNum;
-console.log(output);
+alert(output);
