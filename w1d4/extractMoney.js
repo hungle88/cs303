f79667ccc624e8ba060f871f2bd0883b5b16181b
Hung Le <dangky1410@gmail.com>
2020-09-13 14:25:18 -0500

    lesson 4 assignment update

---
+++ w1d4/extractMoney.js
@@ -0,0 +1,15 @@
+"use strict";
+/**
+ * 
+ * @param {string} Str is the string that needs to be removed the currency symbol and convert into number.
+ * @returns {number} the amount of the money. 
+ */
+function extractCurrencyValue(Str) {
+  let newStr = +str.slice(1);
+  console.log(typeof newStr);
+  return newStr;
+}
+
+let str = "$120";
+let output = extractCurrencyValue(str);
+console.log(output);
