0d094d82babdecda189f8eb341fc10940b2a93ca
Hung Le <dangky1410@gmail.com>
2020-09-13 14:02:53 -0500

    lesson 4 assignment update

---
+++ w1d4/spamCheck.js
@@ -0,0 +1,22 @@
+"use strict";
+/**This function checks if the string value contains "xxx" or "viagra".
+ *
+ * @param {string} str the string that needs to be checked
+ * @returns {boolean} the result is true of false
+ */
+function checkSpam(str) {
+  str = str.toLowerCase();
+  let strArray = str.split(" ");
+  for (let words of strArray) {
+    if (words.includes("viagra") || words.includes("xxx")) {
+      return true;
+    }
+  }
+  return false;
+}
+
+let str = "xXxxx hello";
+// let str ="hello how are you";
+// let str ="buy viAgRA noW";
+
+console.log(checkSpam(str));
