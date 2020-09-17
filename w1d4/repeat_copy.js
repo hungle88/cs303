af71e5a847c28e8d1b5610eddcfd4ceaedc36095
Hung Le <dangky1410@gmail.com>
2020-09-16 22:54:52 -0500

    assignment update

---
+++ w1d4/repeat_copy.js
@@ -0,0 +1,17 @@
+"use strict";
+
+/**
+ * @return {number} the result must be a number
+ */
+function readNumber() {
+  let num;
+  do {
+    num = prompt("Please neter a number: ");
+  } while (!isFinite(num));
+
+  if (num === "" || num === null) {
+    return null;
+  } else {
+    return +num;
+  }
+}
