9c5cd7f820dcaab22749800f44c78a111c9b31ab
Hung Le <dangky1410@gmail.com>
2020-09-16 23:30:39 -0500

    Update randomInteger_test.js

--- w1d4/randomInteger_test.js
+++ w1d4/randomInteger_test.js
@@ -1,21 +1,15 @@
 // "use strict";
 /* global randomInteger assert  */
 describe("randomInteger", function () {
+  it("the random integer is 1", function () {
+    assert.equal(1, randomInteger(1, 3));
+  });
+
   it("the random integer is 2", function () {
-    assert.equal(1, randomInteger(1, 5));
+    assert.equal(2, randomInteger(1, 3));
   });
 
-  it("the random integer is 5", function () {
-    assert.equal(2, randomInteger(1, 5));
-  });
-
-  it("the random integer is 8", function () {
-    assert.equal(3, randomInteger(1, 5));
-  });
-  it("the random integer is 8", function () {
-    assert.equal(4, randomInteger(1, 5));
-  });
-  it("the random integer is 8", function () {
-    assert.equal(5, randomInteger(1, 5));
+  it("the random integer is 3", function () {
+    assert.equal(3, randomInteger(1, 3));
   });
 });
