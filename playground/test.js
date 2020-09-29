d2c14fea2450b3b1a9d70387ba6c099aaa80462a
hungle88 <63668660+hungle88@users.noreply.github.com>
2020-09-28 23:22:18 -0500

    update assignment

--- playground/test.js
+++ playground/test.js
@@ -1,5 +1,17 @@
+<<<<<<< Updated upstream
 "use strict";
 
 Math.floor(min + Math.random() * (max - min + 1));
 Math.floor(0 + Math.random() * (max - 0 + 1));
 Math.floor(0 + Math.random() * (max + 1));
+=======
+let users = [
+    {id: 1, name: "John"},
+    {id: 2, name: "Pete"},
+    {id: 3, name: "Mary"}
+  ];
+  // returns array of the first two users
+  let someUsers = users.filter(item => item.id < 3);
+  
+  console.log(someUsers); 
+>>>>>>> Stashed changes
