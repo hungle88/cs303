04b55a64e594f14fa985a315f92b9041b6199fe8
hungle88 <63668660+hungle88@users.noreply.github.com>
2020-10-03 12:43:41 -0500

    Update test.js

--- playground/test.js
+++ playground/test.js
@@ -1,19 +1,14 @@
-let arr1 =[1, 2, 3, 4, 5];
-
-function even(arr){
-
-    let newArr=[];
-    let j = 0;
-
-    for(let i = 0; i < arr.length; i++){
-        if(arr[i] % 2 === 0) {
-
-            newArr[j] = arr[i];
-            j++;
-        }
-    }
-    return newArr;
-}
-
-let output = even(arr1);
-console.log(output);
+function makeCounter() {
+    let count = 0;
+  
+    return function() {
+      return ++count;
+    };
+  }
+  
+  let counter = makeCounter();
+  console.log(counter());
+  console.log(counter());
+  console.log(counter());
+  console.log(counter());
+  console.log(counter());
