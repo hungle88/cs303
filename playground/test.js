f7fbc6569456bea68b3b474ce95a57d518ad9189
hungle88 <63668660+hungle88@users.noreply.github.com>
2020-10-05 13:21:33 -0500

    Update test.js

--- playground/test.js
+++ playground/test.js
@@ -1,11 +1,12 @@
+"use strict";
 // function makeCounter() {
 //     let count = 0;
-  
+
 //     return function() {
 //       return ++count;
 //     };
 //   }
-  
+
 //   let counter = makeCounter();
 //   console.log(counter());
 //   console.log(counter());
@@ -14,4 +15,35 @@
 //   console.log(counter());
 // console.log(makeCounter);
 
+// let x = 1;
+// function func() {
+//   console.log(x);
+//   x = 2;
+// }
+// fun();
+// func();
 
+// function sayHi() {
+//   console.log('Hello');
+//   }
+//   setTimeout(sayHi, 10000);
+
+function stopWatch(startTime){
+  let currentTime = startTime;
+  const watchId = setInterval(logTime, 1000);
+
+
+  function logTime() {
+
+    console.log(currentTime);
+    if(currentTime === 0) {
+      clearInterval(watchId);
+      // break;
+    } else {
+
+      currentTime = currentTime - 1;
+    }
+  }
+}
+
+stopWatch(6);
