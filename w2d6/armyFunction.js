d3e7f26e263e02c4bdbe7f2ec501c7ece43429ad
Hung Le <dangky1410@gmail.com>
2020-10-06 00:13:33 -0500

    assignment update

--- w2d6/armyFunction.js
+++ w2d6/armyFunction.js
@@ -1,5 +1,5 @@
 "use strict";
-
+{
 console.log("Why do all of the shooters show the same value?");
 console.log(
   "Because the while loop is done before the function shooter is declared, at that time i is already = 10, and the function shooter return i = 10"
@@ -14,14 +14,10 @@ function makeArmy() {
 
   let i = 0;
   while (i < 10) {
-    let j = i;
-    /**
-     * log j into the console
-     * @returns {number} j
-     */
-    let shooter = function () {
-      console.log(j);
-    };
+      let j = i;
+      let shooter = function() { // shooter function
+        alert( j ); // should show its number
+      };
     shooters.push(shooter);
     i++;
   }
@@ -31,7 +27,38 @@ function makeArmy() {
 
 let army = makeArmy();
 
+let army = makeArmy();
+
 army[0]();
 army[1]();
 army[2]();
 
+  /* global army assert  */
+
+describe("army", function() {
+
+  let army;
+  
+  before(function() {
+    army = makeArmy();
+    window.alert = sinon.stub(window, "alert");
+  });
+
+  it("army[0] shows 0", function() {
+    army[0]();
+    assert(alert.calledWith(0));
+  });
+
+
+  it("army[5] shows 5", function() {
+    army[5]();
+    assert(alert.calledWith(5));
+  });
+
+  after(function() {
+    window.alert.restore();
+  });
+
+});
+
+}
