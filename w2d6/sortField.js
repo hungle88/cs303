173d1511f253ae8a663a0f0ef06d5b57120219e5
Hung Le <dangky1410@gmail.com>
2020-10-06 00:25:14 -0500

    assignment update

--- w2d6/sortField.js
+++ w2d6/sortField.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-undef */
 /* eslint-disable id-length */
 "use strict";
+{
 let users = [
     { name: "John", age: 20, surname: "Johnson" },
     { name: "Pete", age: 18, surname: "Peterson" },
@@ -18,3 +19,44 @@ let users = [
   console.log(users.sort(byField("name")));
   console.log(users.sort(byField("age")));
 
+  describe("byField", function(){
+
+    let users = [
+      { name: "John", age: 20, surname: "Johnson" },
+      { name: "Pete", age: 18, surname: "Peterson" },
+      { name: "Ann", age: 19, surname: "Hathaway" },
+    ];
+  
+    it("sorts users by name", function(){
+      let nameSortedKey = [
+        { name: "Ann", age: 19, surname: "Hathaway" },
+        { name: "John", age: 20, surname: "Johnson"},
+        { name: "Pete", age: 18, surname: "Peterson" },
+      ];
+      let nameSortedAnswer = users.sort(byField("name"));
+      assert.deepEqual(nameSortedKey, nameSortedAnswer);
+    });
+  
+    it("sorts users by age", function(){
+      let ageSortedKey = [
+        { name: "Pete", age: 18, surname: "Peterson" },
+        { name: "Ann", age: 19, surname: "Hathaway" },
+        { name: "John", age: 20, surname: "Johnson"},
+      ];
+      let ageSortedAnswer = users.sort(byField("age"));
+      assert.deepEqual(ageSortedKey, ageSortedKey);
+    });
+  
+    it("sorts users by surname", function(){
+      let surnameSortedKey = [
+        { name: "Ann", age: 19, surname: "Hathaway" },
+        { name: "John", age: 20, surname: "Johnson"},
+        { name: "Pete", age: 18, surname: "Peterson" },
+      ];
+      let surnameSortedAnswer = users.sort(byField("surname"));
+      assert.deepEqual(surnameSortedAnswer, surnameSortedKey);
+    });
+  
+  });
+
+}
