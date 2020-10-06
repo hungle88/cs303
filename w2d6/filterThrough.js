a2c571c936ef8d5927ef5be7ef3eae58d3af5524
Hung Le <dangky1410@gmail.com>
2020-10-06 00:51:34 -0500

    update assignment

--- w2d6/filterThrough.js
+++ w2d6/filterThrough.js
@@ -1,54 +1,33 @@
 /* eslint-disable id-length */
 "use strict";
-{
-/**
- *
- * @param {number} a is min value
- * @param {number} b is max value
- * @return {number} numbers between a and b
- */
-function inBetween(a, b) {
-  return function(n) {
-    return n >= a && n <= b;
-  };
-}
 
-/**
- *
- * @param {array} arr is an array of number
- * @return {array} is the filtered array
- */
-function inArray(arr) {
-  return function (n) {
-    return arr.includes(n);
-  };
-}
+  /**
+   *
+   * @param {number} a is min value
+   * @param {number} b is max value
+   * @return {number} numbers between a and b
+   */
+  function inBetween(a, b) {
+    return function (n) {
+      return n >= a && n <= b;
+    };
+  }
 
-let arr = [1, 2, 3, 4, 5, 6, 7];
-
-console.log(arr.filter(inBetween(3, 6)));
-
-console.log(arr.filter(inArray([1, 2, 10])));
+  console.log(arr.filter(inBetween(3, 6)));
 
 
-describe("inArray", function() {
+
+  /**
+   *
+   * @param {array} arr is an array of number
+   * @return {array} is the filtered array
+   */
+  function inArray(arr) {
+    return function (n) {
+      return arr.includes(n);
+    };
+  }
   let arr = [1, 2, 3, 4, 5, 6, 7];
-
-  it("returns the filter for values in array", function() {
-
-    let filter = inArray(arr);
-    assert.isTrue(filter(5));
-    assert.isFalse(filter(0));
-  });
-});
+  console.log(arr.filter(inArray([1, 2, 10])));
 
 
-describe("inBetween", function() {
-
-  it("returns the filter for values between", function() {
-    let filter = inBetween(3, 6);
-    assert.isTrue(filter(5));
-    assert.isFalse(filter(0));
-  });
-});
-}
