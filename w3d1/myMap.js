202e0877c27662567a71c76f35d389c4447d5bf8
Hung Le <dangky1410@gmail.com>
2020-10-06 21:53:30 -0500

    update assignment

--- w3d1/myMap.js
+++ w3d1/myMap.js
@@ -1,5 +1,11 @@
+/* eslint-disable id-length */
 "use strict";
-
+/**
+ * 
+ * @param {array} arr is an array of value
+ * @param {function} func is the callback function
+ * @returns {array} is the mapped array
+ */
 function myMap(arr, func) {
   let newArr = [];
   for (let el of arr) {
@@ -10,11 +16,20 @@ function myMap(arr, func) {
 
 let testArray = [1, 2, 3, 4, 5, 6];
 let testArrayTwo = ["a", "b", "c", "d", "e", "f"];
-
+/**
+ * 
+ * @param {number} num is a number need to be doubled
+ * @returns {number} the doubled number
+ */
 function double(num) {
   return num * 2;
 }
 
+/**
+ * 
+ * @param {string} str is a string value
+ * @returns {string} is the upper-cased string value
+ */
 function upperCase(str) {
   return str.toUpperCase();
 }
--- w3d1/myReduce.js
+++ w3d1/myReduce.js
@@ -1,4 +1,12 @@
+/* eslint-disable id-length */
 "use strict";
+/**
+ * 
+ * @param {array} arr is an array of values
+ * @param {function} func is the callback function
+ * @param {number} init is the initial value
+ * @returns {number} is the result
+ */
 function myReduce(arr, func, init) {
   let accumulator = init;
   for (let el of arr) {
@@ -11,6 +19,12 @@ let testArr = [1, 2, 3, 4, 5];
 
 let output = myReduce(testArr, sum, 2);
 
+/**
+ * 
+ * @param {number} a is a number
+ * @param {number} b is a number
+ * @returns {number} sum of a and b
+ */
 function sum(a, b) {
   return a + b;
 }
