c8b2bde42714eb9b59b546f3e41f7bf459b4890c
Hung Le <dangky1410@gmail.com>
2020-10-06 21:42:17 -0500

    Update myFilter.js

--- w3d1/myFilter.js
+++ w3d1/myFilter.js
@@ -18,7 +18,11 @@ function odd(num) {
   return num % 2 !== 0;
 }
 
+function greaterThanFive(num) {
+  return num > 5;
+}
 let testArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 console.log(myFilter(testArr, even));
 console.log(myFilter(testArr, odd));
+console.log(myFilter(testArr, greaterThanFive));
