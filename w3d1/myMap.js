a8d35cd86e474aedb9bc7298d234926279669267
Hung Le <dangky1410@gmail.com>
2020-10-06 21:45:36 -0500

    Update myMap.js

--- w3d1/myMap.js
+++ w3d1/myMap.js
@@ -1,21 +1,22 @@
 "use strict";
 
 function myMap(arr, func) {
-let newArr = [];
-for(let el of arr) {
+  let newArr = [];
+  for (let el of arr) {
     newArr.push(func(el));
+  }
+  return newArr;
 }
-return newArr;
-}; 
-
-
 
 let testArray = [1, 2, 3, 4, 5, 6];
-
-let output = myMap(testArray, double);
-
-console.log(output);
+let testArrayTwo = ["a", "b", "c", "d", "e", "f"];
 
 function double(num) {
-    return num * 2;
+  return num * 2;
 }
+
+function upperCase(str) {
+  return str.toUpperCase();
+}
+console.log(myMap(testArray, double));
+console.log(myMap(testArrayTwo, upperCase));
