6557eba71c0a95ba70ba587b751b70dfa517558a
hungle88 <63668660+hungle88@users.noreply.github.com>
2020-10-06 19:08:10 -0500

    Update test_2.js

--- playground/test_2.js
+++ playground/test_2.js
@@ -1,5 +1,12 @@
-function sayHi() {
-  console.log('Hello');
+let users = [
+  { name: "John", age: 20, surname: "Johnson" },
+  { name: "Pete", age: 18, surname: "Peterson" },
+  { name: "Ann", age: 19, surname: "Hathaway" }
+];
+
+
+function byField(fieldName){
+  return (a, b) => a.fieldName > b.fieldName ? 1 : -1;
 }
 
-setTimeout(sayHi(), 10000);
+console.log(users.sort(byField("age")));
