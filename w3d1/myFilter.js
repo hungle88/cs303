244b22446661fae12ef1a73a1bcc4ec84a9f77d5
Hung Le <dangky1410@gmail.com>
2020-10-06 21:41:19 -0500

    Update myFilter.js

--- w3d1/myFilter.js
+++ w3d1/myFilter.js
@@ -14,7 +14,11 @@ function even(num) {
   return num % 2 === 0;
 }
 
+function odd(num) {
+  return num % 2 !== 0;
+}
+
 let testArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-let output = myFilter(testArr, even);
-console.log(output);
+console.log(myFilter(testArr, even));
+console.log(myFilter(testArr, odd));
