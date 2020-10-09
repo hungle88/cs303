28a24f9355a9f511456ecd4f31cb1ee4a86b8b2e
Hung Le <dangky1410@gmail.com>
2020-10-09 17:51:45 -0500

    Update doubleTriple.js

--- exam2/doubleTriple.js
+++ exam2/doubleTriple.js
@@ -1,12 +1,11 @@
+/* eslint-disable require-jsdoc */
+/* eslint-disable id-length */
 "use strict";
 
 function foo(bar, apple, banana) {
   if (bar < 100) return apple(bar);
   else return banana(bar);
 }
-
-console.log(foo(100, apple, banana));
-console.log(foo(90, apple, banana));
 
 function apple(n) {
   return n * 2;
@@ -15,3 +14,6 @@ function apple(n) {
 function banana(n) {
   return n * 3;
 }
+
+console.log(foo(100, apple, banana));
+console.log(foo(90, apple, banana));
