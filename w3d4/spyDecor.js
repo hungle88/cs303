068a582a59fb1c5179c7b2224675fd517faad349
Hung Le <dangky1410@gmail.com>
2020-10-09 18:53:13 -0500

    update assignment

--- w3d4/spyDecor.js
+++ w3d4/spyDecor.js
@@ -1,23 +1,22 @@
 "use strict";
 
 function work(a, b) {
-  console.log(a + b); // work is an arbitrary function or method
+  console.log(a + b);
 }
 
 work = spy(work);
 
-work(1, 2); // 3
-work(4, 5); // 9
-work(2, 3); // 5
-work(2, 6); //8
+work(1, 2);
+work(4, 5);
+work(2, 3);
+work(2, 6);
 
 for (let arr of work.calls) {
-  console.log("call:" + arr.join()); // "call:1,2", "call:4,5"
+  console.log("call:" + arr.join());
 }
 
 function spy(func) {
   function wrapper(...arr) {
-    // using ...arr instead of arguments to store "real" array in wrapper.calls
     wrapper.calls.push(arr);
     console.log(wrapper.calls);
 
