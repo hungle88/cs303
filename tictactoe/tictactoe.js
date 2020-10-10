37dc87fbac1051d8f836e3a6af987a03bd2cccbd
Hung Le <dangky1410@gmail.com>
2020-10-10 16:30:50 -0500

    update tictactoe

--- tictactoe/tictactoe.js
+++ tictactoe/tictactoe.js
@@ -1,165 +1,151 @@
-
-var winners = new Array();
-var player1Selections = new Array();
-var player2Selections = new Array();
-var timer;
-var numberOfPlayers = 2;
-var currentPlayer = 0;
-var move = 0;
-var points1 = 0;    // player 1 points
-var points2 = 0;    // player 2 points
-var size = 4;
+/* eslint-disable id-length */
+"use strict";
+let winners = new Array();
+let player1Selections = new Array();
+let player2Selections = new Array();
+let timer;
+let numberOfPlayers = 2;
+let currentPlayer = 0;
+let move = 0;
+let points1 = 0; // player 1 points
+let points2 = 0; // player 2 points
+let size = 4;
 
 function drawBoard() {
-    var Parent = document.getElementById("game");
-    var counter = 1;
-    
-    while (Parent.hasChildNodes()) {
-        Parent.removeChild(Parent.firstChild);
-    }
+  let Parent = document.getElementById("game");
+  let counter = 1;
 
-    for (s = 0; s < 4; s++) {
-        var row = document.createElement("tr");
-        
-        for (r = 0; r < 4; r++) {
-            var col = document.createElement("td");
-            col.id = counter;
+  while (Parent.hasChildNodes()) {
+    Parent.removeChild(Parent.firstChild);
+  }
 
-            var handler = function(e) {
-                if (currentPlayer == 0) {
-                    this.innerHTML = "X";
-                    player1Selections.push(parseInt(this.id));
-                    player1Selections.sort(function(a, b) { return a - b });
-                    d('player1').classList.remove('selected');
-                    d('player2').classList.add('selected');
-                }
+  for (let s = 0; s < 4; s++) {
+    let row = document.createElement("tr");
 
-                else {
-                    this.innerHTML = "O";
-                    player2Selections.push(parseInt(this.id));
-                    player2Selections.sort(function(a, b) { return a - b });
-                    d('player1').classList.add('selected');
-                    d('player2').classList.remove('selected');
-                }
+    for (let r = 0; r < 4; r++) {
+      let col = document.createElement("td");
+      col.id = counter;
 
-                if (checkWinner())
-                {
-                    if(currentPlayer == 0)
-                        points1++;
-                    else
-                        points2++;
-
-                    document.getElementById("player1").innerHTML = points1;
-                    document.getElementById("player2").innerHTML = points2;
-
-                    reset();
-                    drawBoard();
-                }
-
-                else if (player2Selections.length + player1Selections.length == 16)
-                {
-                    reset();
-                    drawBoard();
-                }
-                else
-                {
-                    if (currentPlayer == 0)
-                        currentPlayer = 1;
-                    else
-                        currentPlayer = 0;
-                    this.removeEventListener('click', arguments.callee);
-                }
-            };
-
-            col.addEventListener('click', handler);
-
-            row.appendChild(col);
-            counter++;
+      let handler = function myFunc(e) {
+        if (currentPlayer == 0) {
+          this.innerHTML = "X";
+          player1Selections.push(parseInt(this.id));
+          player1Selections.sort(function (a, b) {
+            return a - b;
+          });
+          d("player1").classList.remove("selected");
+          d("player2").classList.add("selected");
+        } else {
+          this.innerHTML = "O";
+          player2Selections.push(parseInt(this.id));
+          player2Selections.sort(function (a, b) {
+            return a - b;
+          });
+          d("player1").classList.add("selected");
+          d("player2").classList.remove("selected");
         }
 
-        Parent.appendChild(row);
+        if (checkWinner()) {
+          if (currentPlayer == 0) points1++;
+          else points2++;
+
+          document.getElementById("player1").innerHTML = points1;
+          document.getElementById("player2").innerHTML = points2;
+
+          reset();
+          drawBoard();
+        } else if (player2Selections.length + player1Selections.length == 16) {
+          reset();
+          drawBoard();
+        } else {
+          if (currentPlayer == 0) currentPlayer = 1;
+          else currentPlayer = 0;
+          return this.removeEventListener("click", myFunc());
+        }
+      };
+
+      col.addEventListener("click", handler);
+
+      row.appendChild(col);
+      counter++;
     }
 
-    loadAnswers();
+    Parent.appendChild(row);
+  }
+
+  loadAnswers();
 }
 
-function d(id)
-{
-    var el = document.getElementById(id);
-    return el;
+function d(id) {
+  let el = document.getElementById(id);
+  return el;
 }
-function reset()
-{
-    currentPlayer = 0;
-    player1Selections = new Array();
-    player2Selections = new Array();
-    d('player1').classList.add('selected');
-    d('player2').classList.remove('selected');
+function reset() {
+  currentPlayer = 0;
+  player1Selections = new Array();
+  player2Selections = new Array();
+  d("player1").classList.add("selected");
+  d("player2").classList.remove("selected");
 }
 
-function loadAnswers()
-{
-    winners.push([1, 2, 3, 4]);
-    winners.push([5, 6, 7, 8]);
-    winners.push([9, 10, 11, 12]);
-    winners.push([13, 14, 15, 16]);
-    winners.push([1, 5, 9, 13]);
-    winners.push([2, 6, 10, 14]);
-    winners.push([3, 7, 11, 15]);
-    winners.push([4, 8, 12, 16]);
-    winners.push([1, 6, 11, 16]);
-    winners.push([4, 7, 10, 13]);
-
- 
+function loadAnswers() {
+  winners.push([1, 2, 3, 4]);
+  winners.push([5, 6, 7, 8]);
+  winners.push([9, 10, 11, 12]);
+  winners.push([13, 14, 15, 16]);
+  winners.push([1, 5, 9, 13]);
+  winners.push([2, 6, 10, 14]);
+  winners.push([3, 7, 11, 15]);
+  winners.push([4, 8, 12, 16]);
+  winners.push([1, 6, 11, 16]);
+  winners.push([4, 7, 10, 13]);
 }
 
 function checkWinner() {
-    // check if current player has a winning hand
-    // only stsrt checking when player x has size number of selections
-    var win = false;
-    var playerSelections = new Array();
+  // check if current player has a winning hand
+  // only stsrt checking when player x has size number of selections
+  let win = false;
+  let playerSelections = new Array();
 
-    if (currentPlayer == 0)
-        playerSelections = player1Selections;
-    else
-	playerSelections = player2Selections;
-    
-    if (playerSelections.length >= size) {
-        // check if any 'winners' are also in your selections
-        
-        for (i = 0; i < winners.length; i++) {
-            var sets = winners[i];  // winning hand
-            var setFound = true;
-            
-            for (r = 0; r < sets.length; r++) {
-                // check if number is in current players hand
-                // if not, break, not winner
-                var found = false;
-                
-                // players hand
-                for (s = 0; s < playerSelections.length; s++) {
-                    if (sets[r] == playerSelections[s]) {
-                        found = true;
-                        break;
-                    }
-                }
+  if (currentPlayer == 0) playerSelections = player1Selections;
+  else playerSelections = player2Selections;
 
-                // value not found in players hand
-                // not a valid set, move on
-                if (found == false) {
-                    setFound = false;
-                    break;
-                }
-            }
+  if (playerSelections.length >= size) {
+    // check if any 'winners' are also in your selections
 
-            if (setFound == true) {
-                win = true;
-                break;
-            }
+    for (let i = 0; i < winners.length; i++) {
+      let sets = winners[i]; // winning hand
+      let setFound = true;
+
+      for (let r = 0; r < sets.length; r++) {
+        // check if number is in current players hand
+        // if not, break, not winner
+        let found = false;
+
+        // players hand
+        for (let s = 0; s < playerSelections.length; s++) {
+          if (sets[r] == playerSelections[s]) {
+            found = true;
+            break;
+          }
         }
+
+        // value not found in players hand
+        // not a valid set, move on
+        if (found == false) {
+          setFound = false;
+          break;
+        }
+      }
+
+      if (setFound == true) {
+        win = true;
+        break;
+      }
     }
+  }
 
-    return win;
-} 
+  return win;
+}
 
-window.addEventListener('load', drawBoard);
+window.addEventListener("load", drawBoard);
