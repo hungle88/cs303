3c452e7075141120713218e7afa3484d8cfc705c
Hung Le <dangky1410@gmail.com>
2020-10-13 17:40:29 -0500

    Update test.js

--- playground/test.js
+++ playground/test.js
@@ -1,15 +1,37 @@
-class User {
-  constructor(name = "Anonymous") {
+class Animal {
+  constructor(name) {
+  this.speed = 0;
   this.name = name;
   }
-  sayHi() {
-  console.log(`Hello, ${this.name}!`);
+  run(speed) {
+  this.speed += speed;
+  alert(`${this.name} runs with speed ${this.speed}.`);
+  }
+  stop() {
+  this.speed = 0;
+  alert(`${this.name} stands still.`);
   }}
-  fred = new User();
-  console.log(fred);
-  console.log(fred.__proto__);
-  console.log(User.prototype);
-  fred.sayHi();
-  bob = new User("Bob");
-  console.log(bob);
-  bob.sayHi();
+  class Rabbit extends Animal {
+    constructor(name, earLength) {
+      super(name);
+      this.earLength = earLength;
+      }
+  hide() {
+  alert(`${this.name} hides!`);
+  }
+  stop() {
+  super.stop(); // call parent stop
+  this.hide(); // and then hide
+  }
+
+ear(){
+  alert("the ears are "+this.earLength + " inches!")
+}}
+
+  
+   
+  let rabbit = new Rabbit("White Rabit", 6);
+  rabbit.run(5);
+  rabbit.hide();
+  rabbit.stop();
+  rabbit.ear();
