ff21318271bc237f90d0a58e4d91d6270b97a402
hungle88 <63668660+hungle88@users.noreply.github.com>
2020-10-16 11:03:56 -0500

    update error handling

---
+++ w4d6/finally2.js
@@ -0,0 +1,10 @@
+function func() {
+    try {
+        return 1;
+    } catch (e) {
+        /* ... */
+    } finally {
+        console.log('finally');
+    }
+}
+console.log(func()); // first works alert from finally, and then this one
