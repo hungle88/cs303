f6bf8954477725be646f93162588a3d0de83cd0c
Hung Le <dangky1410@gmail.com>
2020-10-16 20:08:55 -0500

    Update test.js

--- playground/test.js
+++ playground/test.js
@@ -1,12 +1,34 @@
-function Person(name){
-    console.log(this);
-    this.name = name;
-    console.log("hello",this);
-
-;}
-
-function Doctor(name, dept){
-    console.log(this);
-    Person.call(this, name, dept);
-    console.log(this)
+function Person(name) {
+  console.log(this);
+  this.name = name;
+  console.log(this);
 }
+
+function Doctor(name, dept) {
+  console.log(this);
+  Person.call(this, name, dept);
+  console.log(this);
+  this.dept = dept;
+  console.log(this);
+}
+
+Doctor.__proto__.report = function () {
+  console.log("I'm going to die");
+};
+
+function Surgeon(name, dept) {
+  console.log(this);
+  Doctor.call(this, name, dept);
+  console.log(this);
+  this.dept = dept;
+  console.log(this);
+}
+
+Surgeon.__proto__.operate = function () {
+  console.log("HAHAHAHAHAHA");
+};
+
+let surg1 = new Surgeon("Hung", "killer");
+surg1.operate();
+console.log(surg1);
+console.log(surg1.prototype);
